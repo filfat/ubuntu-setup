668f177965e2bddb6d438d4e51c268d3af267e71
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2019-01-08 02:12:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,11 +22,11 @@ mkdir ~/.themes
 mkdir ~/.fonts
 
 wget https://dl.opendesktop.org/api/files/download/id/1546860792/s/e9b3577dc997e00561725bb5f3d7d7ad0a17fd6c54ffefa8744ad2fb70af8b0f38c5398d339a6dca631a7a32a0fb18f048b752d49a8302d523bcbe5c58f65ce4/t/1546911671/u//Mojave-dark-solid.tar.xz
-tar -xf Mojave-dark-solid.tar.xz -C ~/.themes
+sudo tar -xf Mojave-dark-solid.tar.xz -C ~/.themes
 rm -rf Mojave-dark-solid.tar.xz
 
 wget https://dl.opendesktop.org/api/files/download/id/1545202506/s/1cd7e34ffa12ee4a093f8a77821fd17fb358642c0852cdb423ff880e1c14549439aeb192e6f7e97cf7df036690ec7963360d58e01a8f7dc9ee8aa10418cb7c3a/t/1546912396/u//macOS.tar.xz
-tar -xf macOS.tar.xz ~/.icons
+sudo tar -xf macOS.tar.xz -C~/ .icons
 rm -rf macOS.tar.xz
 
 git clone https://github.com/AppleDesignResources/SanFranciscoFont ~/.fonts
@@ -39,5 +39,10 @@ gsettings set org.gnome.desktop.interface icon-theme "MacOS"
 
 gsettings set org.gnome.desktop.interface document-font-name "San Francisco Text 11"
 gsettings set org.gnome.desktop.interface font-name "San Francisco Text 11"
+
+gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
 
 echo Done!
