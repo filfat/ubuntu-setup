3430a6c68003acfadec11889261a3d3004e6180b
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2019-01-08 02:07:14 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo Adding keys and repos...
+sudo apt install -y curl
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
