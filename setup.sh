62725480fdd0623b42cff8a79ae9812c8e17e2b3
Filiph Sandstrom <filiph.sandstrom@filfatstudios.com>
2019-01-08 02:05:44 +0100

    Add missing things...

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo apt upgrade -y
 sudo apt full-upgrade -y
 
 echo Installing tools...
-sudo apt install -y git docker build-essential clang yarn wget
+sudo apt install -y git docker build-essential clang yarn wget gnome-tweak-tool
 
 echo Installing C++ libraries...
 sudo apt install -y libsdl2-dev libsdl2-ttf-dev
@@ -19,10 +19,24 @@ echo Setting up icons, theme and fonts...
 mkdir ~/.icons
 mkdir ~/.themes
 mkdir ~/.fonts
-# TODO: wget theme, icons & font
+
+wget https://dl.opendesktop.org/api/files/download/id/1546860792/s/e9b3577dc997e00561725bb5f3d7d7ad0a17fd6c54ffefa8744ad2fb70af8b0f38c5398d339a6dca631a7a32a0fb18f048b752d49a8302d523bcbe5c58f65ce4/t/1546911671/u//Mojave-dark-solid.tar.xz
+tar -xf Mojave-dark-solid.tar.xz -C ~/.themes
+rm -rf Mojave-dark-solid.tar.xz
+
+wget https://dl.opendesktop.org/api/files/download/id/1545202506/s/1cd7e34ffa12ee4a093f8a77821fd17fb358642c0852cdb423ff880e1c14549439aeb192e6f7e97cf7df036690ec7963360d58e01a8f7dc9ee8aa10418cb7c3a/t/1546912396/u//macOS.tar.xz
+tar -xf macOS.tar.xz ~/.icons
+rm -rf macOS.tar.xz
+
+git clone https://github.com/AppleDesignResources/SanFranciscoFont ~/.fonts
+rm -rf ~/.fonts/.git
 
 echo Configuring GNOME...
-# TODO: Configure window action buttons
-# TODO: Confugure theme
-# TODO: Configure icons
-# TODO: Configure fonts
+gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
+gsettings set org.gnome.desktop.interface gtk-theme "Mojave-dark-solid"
+gsettings set org.gnome.desktop.interface icon-theme "MacOS"
+
+gsettings set org.gnome.desktop.interface document-font-name "San Francisco Text 11"
+gsettings set org.gnome.desktop.interface font-name "San Francisco Text 11"
+
+echo Done!
